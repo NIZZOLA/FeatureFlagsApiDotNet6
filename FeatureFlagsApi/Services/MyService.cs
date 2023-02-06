f5eddd9e2af8193ea585f2dd5a87fae1b08f18f8
Marcio Rogerio Nizzola <marcio@nizzola.com.br>
2023-02-05 21:46:14 -0300

    Add project files.

---
+++ FeatureFlagsApi/Services/MyService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.FeatureManagement;
+
+namespace FeatureFlagsApi.Services
+{
+    public class MyService
+    {
+        private readonly IFeatureManager _featureManager;
+        public MyService(IFeatureManagerSnapshot featureManager)
+        {
+            _featureManager = featureManager;
+        }
+        public async Task<string> Process()
+        {
+            // Verifica se o recurso "NewMethod" está habilitado
+            if (await _featureManager.IsEnabledAsync("NewMethod"))
+            {
+                Console.WriteLine("Executando o método novo...");
+                return NewMethod();
+            }
+            else
+            {
+                Console.WriteLine("Executando o método legado...");
+                return LegacyMethod();
+            }
+        }
+
+        public string NewMethod()
+        {
+            return "Novo Método";
+        }
+        public string LegacyMethod()
+        {
+            return "Método legado";
+        }
+    }
+}
